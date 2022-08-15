4226662ca414415d9426b3e6f2f29a1b8df31856
Vivekbisht84 <54701285+Vivekbisht84@users.noreply.github.com>
2022-08-15 09:38:01 +0530

    Add files via upload

---
+++ DuplicateN+1Integer.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int Duplicate(int arr[] , int n){
+    int slow = arr[0];
+    int fast = arr[0];
+
+    do{
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    }while(slow!=fast);
+
+    fast = arr[0];
+    while(slow!=fast){
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+    return slow;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ; i < n ; i++){
+        cin>>arr[i];
+    }
+    int ans = Duplicate(arr,n);
+    cout<<ans<<endl;
+}
