690299c7dd579e60d9f5868afa65524b2e4622fe
Vivekbisht84 <54701285+Vivekbisht84@users.noreply.github.com>
2022-08-16 20:13:38 +0530

    Add files via upload

---
+++ SingleElement.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int SingleElement(int arr[] , int n){
+    int s = 0;
+    int e = n - 1;
+    while(s<=e){
+        int mid = s + (e - s)/2;
+
+        if(arr[mid] == arr[mid ^ 1]){
+            s = mid + 1;
+        }
+        else {
+            e = mid - 1;
+        }
+    }return arr[s];
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ; i < n ; i++){
+        cin>>arr[i];
+    }
+    int ans = SingleElement(arr,n);
+    cout<<ans<<endl;
+}
